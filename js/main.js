07aa498ee7b0958fbef78668feba33c25c7491e3
mehedi-h <mehedihs2015@gmail.com>
2023-08-29 20:25:03 +0600

    add dynamic data

--- js/main.js
+++ js/main.js
@@ -2,18 +2,46 @@
 const loadAiNews = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/ai/tools');
     const data = await res.json()
-    const aiDatas = data.data
+    console.log(data)
+    const aiDatas = data.data.tools
 
+    // console.log(aiDatas)
     displayAiData(aiDatas)
-    // console.log(data)
 }
+const displayAiData = (aiDatas) => {
+    console.log(aiDatas)
 
-const displayAiData = aiDatas => {
-    const aiNewsContainer = document.getElementById('ai-news-container');
+    const aiNewsContainer = document.getElementById('ai-news-container')
+    aiNewsContainer.textContent = '';
     
-    aiDatas.forEach(data => {
-        console.log(data)
-    })
+    aiDatas.forEach(aiData => {
+        // console.log(element)
+
+        const aiNewsCard = document.createElement('div')
+        aiNewsCard.classList = `card bg-slate-100 shadow-xl`
+
+        aiNewsCard.innerHTML = `
+            <div>
+                <figure class="px-5 pt-5">
+                    <img src="${aiData.image}" alt="" class="rounded-xl" />
+                </figure>
+                <div class="card-body text-justify">
+                    <h2 class="card-title text-xl font-bold">Features :</h2>
+                    <div class="pb-5">
+                        <li>${aiData.features[0]}</li>
+                        <li>${aiData.features[1]}</li>
+                        <li>${aiData.features[2]}</li>
+                    </div>
+                    <hr class="">
+                    <h2 class="card-title text-xl font-bold pt-3">${aiData.name}</h2>
+                    <p>${aiData.published_in}</p>
+                </div>
+            </div>
+        `
+        //--------------- Append Child ------------------------>
+        aiNewsContainer.appendChild(aiNewsCard)
+    });
+
 }
 
 loadAiNews()
